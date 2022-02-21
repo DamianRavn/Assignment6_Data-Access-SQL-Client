6d2f046ea8c6469ab1fd714b57c4106d65ff032d
dragonalias <dragon1600@gmail.com>
2022-02-21 17:44:12 +0100

    feat: reading from sql database WIP

---
+++ ProjectB/Repositories/ConnectionStringHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ProjectB.Repositories
+{
+    public class ConnectionStringHelper
+    {
+        public static string GetConnectionString()
+        {
+            SqlConnectionStringBuilder connectionStringBuilder = new SqlConnectionStringBuilder();
+            connectionStringBuilder.DataSource = "STATIONARYDRAGO\\SQLEXPRESS";
+            connectionStringBuilder.InitialCatalog = "Chinook";
+            connectionStringBuilder.IntegratedSecurity = true;
+            return connectionStringBuilder.ConnectionString;
+        }
+    }
+}
