25700d78e3f637e5c9af0980472d6de34ea6d42a
dragonalias <dragon1600@gmail.com>
2022-02-23 09:01:00 +0100

    feat: generic repository

---
+++ ProjectB/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using ProjectB.Models;
+using System.Collections.Generic;
+
+namespace ProjectB.Repositories
+{
+    public interface IRepository<T>
+    {
+        public T GetById(int id);
+
+        public List<T> GetAll();
+
+        public bool Add(T entity);
+
+        public bool Update(T entity);
+
+        public bool Delete(T entity);
+
+    }
+}
